91955e479cf41809aea2ea2b89e649b99ea09ef5
ZedroXxXx <noahbeugnet.pro@gmail.com>
2023-12-15 11:49:41 +0100

    cestbon

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-swfesesegsefr
+<?php
+$servername = "localhost";
+$dbname = "cabinet";
+$username = "etu";
+$password = "iutinfo";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Définit le mode d'erreur PDO sur exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie";
+} catch(PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>  

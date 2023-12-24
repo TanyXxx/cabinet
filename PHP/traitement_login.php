c5f463560e060b4610b88c16fa938d5824f18353
Soltan Hamadouche <soltan.hamadouche@gmail.com>
2023-12-24 23:29:38 +0100

    logout

--- PHP/traitement_login.php
+++ PHP/traitement_login.php
@@ -13,12 +13,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($stmt->rowCount() == 1) {
         $user = $stmt->fetch();
-        if (password_verify($password, $user['MotDePasse'])) {
+
+        if ($password === $user['MotDePasse']) {
             $_SESSION['loggedin'] = true;
             $_SESSION['user_id'] = $user['ID_Utilisateur'];
             $_SESSION['username'] = $user['NomUtilisateur'];
 
-            header("Location: index.php");
+            header("Location: ../index.php");
             exit;
         } else {
             $error = "Identifiants incorrects";
---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Détruire toutes les variables de session
+$_SESSION = array();
+
+// Détruire la session
+session_destroy();
+
+// Rediriger vers la page de connexion
+header("Location: login.php");
+exit;
+?>

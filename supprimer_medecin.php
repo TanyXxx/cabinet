78bfd360a0f37db505eaf532054052e71a5f6da3
ZedroXxXx <noahbeugnet.pro@gmail.com>
2024-01-17 14:39:34 +0100

    avancee

--- supprimer_medecin.php
+++ supprimer_medecin.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
         echo "<h2>Confirmez-vous la suppression du médecin ID: " . htmlspecialchars($id) . " ?</h2>";
-        echo "<a href='/cabinet/cabinet-1/php/traitement_supprimer_medecin.php?id=" . $id . "'>Confirmer la Suppression</a>";
+        echo "<a href='/cabinet/php/traitement_supprimer_medecin.php?id=" . $id . "'>Confirmer la Suppression</a>";
     } else {
         echo "<p>ID de médecin non spécifié.</p>";
     }
--- supprimer_usager.php
+++ supprimer_usager.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
         echo "<h2>Confirmez-vous la suppression de l'usager ID: " . htmlspecialchars($id) . " ?</h2>";
-        echo "<a href='/cabinet/cabinet-1/php/traitement_supprimer_usager.php?id=" . $id . "'>Confirmer la Suppression</a>";
+        echo "<a href='/cabinet/php/traitement_supprimer_usager.php?id=" . $id . "'>Confirmer la Suppression</a>";
     } else {
         echo "<p>ID d'usager non spécifié.</p>";
     }
